ba851f4e0bc2fe7cbdbc6410dc17a0b5ff5cbe77
Sniperovitch <sniperovitch@gmail.com>
2017-02-08 22:22:39 +0100

    build pandoc from source, debian version lack of --filter

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+cd $HOME
+wget https://github.com/jgm/pandoc/releases/download/1.19.2.1/pandoc-1.19.2.1-1-amd64.deb
+dpkg -i *.deb
+git clone https://github.com/hugoroy/exegetesDoc.git
+cd $HOME/cocktail
 cp -v cocktail.conf.smp cocktail.conf
-cd && git clone https://github.com/hugoroy/exegetesDoc.git
-cd -
 
