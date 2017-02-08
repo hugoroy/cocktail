d05f37c9dd8c3f76f940c495b27e4ab7ec5b10de
Sniperovitch <sniperovitch@gmail.com>
2017-02-08 22:30:24 +0100

    encore un essai de test build dans travis

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-cd $HOME
-wget https://github.com/jgm/pandoc/releases/download/1.19.2.1/pandoc-1.19.2.1-1-amd64.deb
-dpkg -i *.deb
-git clone https://github.com/hugoroy/exegetesDoc.git
-cd $HOME/cocktail
+cd && git clone https://github.com/hugoroy/exegetesDoc.git
+cd -
 cp -v cocktail.conf.smp cocktail.conf
 
