9e0e9f9cbebcdb9959763eb2809d7f38bcfd5b63
Sniperovitch <sniperovitch@gmail.com>
2017-02-11 23:06:39 +0100

    URL encode/decode les addresses des pads

--- mirror_pad.pl
+++ mirror_pad.pl
@@ -6,8 +6,9 @@ use Etherpad;
 use Data::Dumper;
 use Encode ();
 use FindBin;
+use URI::Encode ();
 
-my $url = shift;
+my $url = URI::Encode::uri_decode(shift);
 my $c = Config::YAML->new(config => "$FindBin::Bin/mirror_pad.yml");
 my $ep = Etherpad->new(
   url      => $c->get_url,
@@ -18,7 +19,13 @@ my $ep = Etherpad->new(
 
 my $pad_base = quotemeta $c->get_url;
 my $pad_id = $url;
-$pad_id =~ s{$pad_base/?.+?/(g\.[^$]+\$[^/].+)}{$1};
-my $pad_text = Encode::encode('UTF-8', $ep->get_text($pad_id) );
-print $pad_text;
+$pad_id =~ s{$pad_base/.+?/(g\.[^/]+).*$}{$1};
+warn "PADID=$pad_id\n";
+if(my($pad_text) = $ep->get_text($pad_id)) {
+    $pad_text = Encode::encode('UTF-8', $pad_text);
+    print $pad_text;
+}
+else {
+    exit 1;
+}
 
