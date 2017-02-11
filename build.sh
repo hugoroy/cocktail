a84fa87ee58fe0e9c4882e5e108815514ea5c9de
Sniperovitch <sniperovitch@gmail.com>
2017-02-12 00:14:18 +0100

    installons cpanm à la main

--- build.sh
+++ build.sh
@@ -2,4 +2,9 @@
 cd && git clone https://github.com/hugoroy/exegetesDoc.git
 cd -
 cp -v cocktail.conf.smp cocktail.conf
+curl -L https://cpanmin.us | perl - App::cpanminus
+~/perl5/bin/cpanm local::lib
+~/perl5/bin/cpanm URI::Encode Etherpad Config::YAML
 
+# Add path to ~/perl5 modules to ~/.bashrc
+echo '[ $SHLVL -eq 1 ] && eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)"' >> ~/.bashrc
