94b2e4cb5a06c0a6d90a82536f83bdd89990aea6
Sniperovitch <sniperovitch@gmail.com>
2017-02-13 21:17:00 +0100

    local::lib pour les installs

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd -
 cp -v cocktail.conf.smp cocktail.conf
 curl -L https://cpanmin.us | perl - App::cpanminus
 ~/perl5/bin/cpanm local::lib
+cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
+
 ~/perl5/bin/cpanm URI::Encode Etherpad Config::YAML
 
-# Add path to ~/perl5 modules to ~/.bashrc
-echo '[ $SHLVL -eq 1 ] && eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)"' >> ~/.bashrc
