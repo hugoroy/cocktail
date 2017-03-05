471e4b008979489092f194e7a66b4d99345a0a29
David Sniperovitch <david@sniperovitch.fr>
2017-03-05 18:23:42 +0100

    build.sh error

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ cd && git clone https://github.com/hugoroy/exegetesDoc.git
 cd -
 cp -v cocktail.conf.smp cocktail.conf
 curl -L https://cpanmin.us | perl - App::cpanminus
-~/perl5/bin/cpanm --local-lib=~/perl5 local::lib
+cpanm --local-lib=~/perl5 local::lib
+eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
 perl -I ~/perl5/lib/perl5/ -Mlocal::lib >> .bashrc
-. ./.bashrc
 ~/perl5/bin/cpanm URI::Encode Etherpad Config::YAML IO::Socket::SSL
 
