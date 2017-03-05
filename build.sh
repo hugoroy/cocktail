4591893ea6700f1afa8c58d77cf62c20dc8bee15
David Sniperovitch <david@sniperovitch.fr>
2017-03-05 18:50:57 +0100

    no test

--- build.sh
+++ build.sh
@@ -2,9 +2,7 @@
 cd && git clone https://github.com/hugoroy/exegetesDoc.git
 cd -
 cp -v cocktail.conf.smp cocktail.conf
+cp -v mirror_pad.yml.smp mirror_pad.yml
 curl -L https://cpanmin.us | perl - App::cpanminus
-# cpanm --local-lib=~/perl5 local::lib
-# eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
-# perl -I ~/perl5/lib/perl5/ -Mlocal::lib >> .bashrc
-cpanm URI::Encode Etherpad Config::YAML IO::Socket::SSL
+cpanm --no-test URI::Encode Etherpad Config::YAML IO::Socket::SSL
 
