f0c4aaf4dd94102dd2507cf06c2f2dc405852056
David Sniperovitch <david@sniperovitch.fr>
2017-03-05 19:28:58 +0100

    require LWP::UserAgent

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ cd -
 cp -v cocktail.conf.smp cocktail.conf
 cp -v mirror_pad.yml.smp mirror_pad.yml
 curl -L https://cpanmin.us | perl - App::cpanminus
-cpanm --no-test URI::Encode Etherpad Config::YAML IO::Socket::SSL
+cpanm --no-test URI::Encode Etherpad Config::YAML IO::Socket::SSL LWP::UserAgent
 
