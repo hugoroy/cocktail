dcc429a07347321e2e3771e8b7652609854cbb0c
David Sniperovitch <david@sniperovitch.fr>
2017-03-08 00:28:05 +0100

    mirrorpad FatPacker

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-cd && git clone https://github.com/hugoroy/exegetesDoc.git
-cd && git clone https://github.com/sniperovitch/mirrorpad.git
-cd $HOME/mirrorpad/
-cpanm Carton
-carton install
-cd $HOME/cocktail/
 cp -v cocktail.conf.smp cocktail.conf
+cd
+git clone https://github.com/hugoroy/exegetesDoc.git
+git clone https://github.com/sniperovitch/mirrorpad.git
+cp $HOME/mirrorpad/mirrorpad.yml.smp $HOME/mirrorpad/mirrorpad.yml
+cd -
 
