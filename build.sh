84421c3dc0a919d8eaea02c90ab561ae4df356b3
David Sniperovitch <david@sniperovitch.fr>
2017-03-08 00:08:55 +0100

    mirrorpad est un repo externe maintenant

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 cd && git clone https://github.com/hugoroy/exegetesDoc.git
 cd -
+cd && git clone https://github.com/sniperovitch/mirrorpad.git
+cd -
 cp -v cocktail.conf.smp cocktail.conf
-cp -v mirror_pad.yml.smp mirror_pad.yml
-curl -L https://cpanmin.us | perl - App::cpanminus
-cpanm --no-test URI::Encode Etherpad Config::YAML IO::Socket::SSL LWP::UserAgent
 
