0a5f394a649b4fa44ca513a7457a6df0e005e728
David Sniperovitch <david@sniperovitch.fr>
2017-03-08 00:13:42 +0100

    config travis

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 cd && git clone https://github.com/hugoroy/exegetesDoc.git
-cd -
 cd && git clone https://github.com/sniperovitch/mirrorpad.git
-cd -
+cd mirrorpad/
+carton install
+cd
 cp -v cocktail.conf.smp cocktail.conf
 
