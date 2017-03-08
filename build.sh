1d51947e4b672d63b86fa54ee3a6ec9d3d9bb213
David Sniperovitch <david@sniperovitch.fr>
2017-03-08 01:17:40 +0100

    dependances mirrorpad

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ cd
 git clone https://github.com/hugoroy/exegetesDoc.git
 git clone https://github.com/sniperovitch/mirrorpad.git
 cp $HOME/mirrorpad/mirrorpad.yml.smp $HOME/mirrorpad/mirrorpad.yml
+$HOME/mirrorpad/build.sh
 cd -
 
