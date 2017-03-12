940304179e3dae8502c9e85badb4c80fb4180df8
David Sniperovitch <david@sniperovitch.fr>
2017-03-12 15:22:25 +0100

    travis dans build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
+
+# Install Pandoc from sources (Debian version is without filters)
+wget https://github.com/jgm/pandoc/releases/download/1.19.2.1/pandoc-1.19.2.1-1-amd64.deb
+sudo dpkg -i *.deb
+
+sudo apt-get -qq update
+sudo apt-get install -y texlive-latex-base texlive-font-utils texlive-latex-recommended texlive-fonts-recommended texlive-latex-extra
+sudo apt-get install -y python-pip
+sudo pip install pandoc-latex-environment
+
 cp -v cocktail.conf.smp cocktail.conf
 cd
 git clone https://github.com/hugoroy/exegetesDoc.git
 git clone https://github.com/sniperovitch/mirrorpad.git
-cp $HOME/mirrorpad/mirrorpad.yml.smp $HOME/mirrorpad/mirrorpad.yml
-#sudo $HOME/mirrorpad/build.sh
+$HOME/mirrorpad/build.sh
 cd -
 
